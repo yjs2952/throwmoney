d4b6816236ac51f3437aac9d178041ccf189a2b4
yjs2952 <yjs2952@naver.com>
2020-11-21 18:24:38 +0900

    feature: 초기 데이터 insert

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into member (name, create_date, modified_date) values ('라이언', now(), now());
+insert into member (name, create_date, modified_date) values ('무지', now(), now());
+insert into member (name, create_date, modified_date) values ('어피치', now(), now());
+insert into member (name, create_date, modified_date) values ('프로도', now(), now());
+insert into member (name, create_date, modified_date) values ('콘', now(), now());
+insert into member (name, create_date, modified_date) values ('튜브', now(), now());
+insert into member (name, create_date, modified_date) values ('제이지', now(), now());
+insert into member (name, create_date, modified_date) values ('네오', now(), now());
+
+insert into money (total_amount, member_id) values (100000, 1);
+insert into money (total_amount, member_id) values (90000, 2);
+insert into money (total_amount, member_id) values (50000, 3);
+insert into money (total_amount, member_id) values (5000, 4);
+insert into money (total_amount, member_id) values (10000, 5);
+insert into money (total_amount, member_id) values (6743, 6);
+insert into money (total_amount, member_id) values (8910, 7);
+insert into money (total_amount, member_id) values (79997, 8);
