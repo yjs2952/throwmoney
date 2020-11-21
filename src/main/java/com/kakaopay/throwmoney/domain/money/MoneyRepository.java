902aeeafec7719bf686672e4aaddf60d6cb72af6
yjs2952 <yjs2952@naver.com>
2020-11-21 18:59:09 +0900

    feature: 도메인 별 JpaRepository 생성

---
+++ src/main/java/com/kakaopay/throwmoney/domain/money/MoneyRepository.java
@@ -0,0 +1,7 @@
+package com.kakaopay.throwmoney.domain.money;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MoneyRepository extends JpaRepository<Money, Long> {
+
+}
